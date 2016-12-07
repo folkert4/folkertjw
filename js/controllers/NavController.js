9fba0b0e3905bbb0a6b971d5ee17226e18313e0e
folkert4 <folkertjw@gmail.com>
2016-12-06 19:03:29 -0500

    content changes
    removal of unneeded complexity in the email page.

--- js/controllers/NavController.js
+++ js/controllers/NavController.js
@@ -1,6 +1,5 @@
 app.controller('NavController', function($scope, $location){ 
     $scope.isActive = function (viewLocation) {
-        console.log($location.path(), viewLocation, viewLocation === $location.path());
         return viewLocation === $location.path();
     };
 });
